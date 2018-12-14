01e6bfe774a6fcae5971dced773058036ff3647f
Karol Kruzelecki <karol@kruzelecki.com>
2018-12-14 20:38:28 +0100

    LeetCode: 427 - construct a quad tree

---
+++ leetcode/427_construct_quad_tree.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+class Solution:
+    # def go(self, size, y, x):
+    #     values = [
+    #         self.grid[i][j] for i in range(y, y + size) for j in range(x, x + size)
+    #     ]
+    #     sum_of = sum(values)
+    #     if sum_of == 0:
+    #         return Node(
+    #             val=False,
+    #             isLeaf=True,
+    #             topLeft=None,
+    #             topRight=None,
+    #             bottomLeft=None,
+    #             bottomRight=None,
+    #         )
+    #     elif sum_of == len(values):
+    #         return Node(
+    #             val=True,
+    #             isLeaf=True,
+    #             topLeft=None,
+    #             topRight=None,
+    #             bottomLeft=None,
+    #             bottomRight=None,
+    #         )
+    #     else:
+    #         s = size // 2
+    #         return Node(
+    #             val=None,
+    #             isLeaf=False,
+    #             topLeft=self.go(s, y, x),
+    #             topRight=self.go(s, y, x + s),
+    #             bottomLeft=self.go(s, y + s, x),
+    #             bottomRight=self.go(s, y + s, x + s),
+    #         )
+
+    def go(self, size, y, x):  # list of values replaced with a set
+        values = set(
+            self.grid[i][j] for i in range(y, y + size) for j in range(x, x + size)
+        )
+        if len(values) == 1:
+            if values.pop() == 0:
+                return Node(
+                    val=False,
+                    isLeaf=True,
+                    topLeft=None,
+                    topRight=None,
+                    bottomLeft=None,
+                    bottomRight=None,
+                )
+            else:
+                return Node(
+                    val=True,
+                    isLeaf=True,
+                    topLeft=None,
+                    topRight=None,
+                    bottomLeft=None,
+                    bottomRight=None,
+                )
+        else:
+            s = size // 2
+            return Node(
+                val=None,
+                isLeaf=False,
+                topLeft=self.go(s, y, x),
+                topRight=self.go(s, y, x + s),
+                bottomLeft=self.go(s, y + s, x),
+                bottomRight=self.go(s, y + s, x + s),
+            )
+
+    def construct(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: Node
+        """
+        size = len(grid)
+        self.grid = grid
+        return self.go(size, 0, 0)
+
+
+def show(node, lvl=0):
+    if node:
+        print(" " * 4 * lvl + str(node.val))
+        show(node.topLeft, lvl + 1)
+        show(node.topRight, lvl + 1)
+        show(node.bottomLeft, lvl + 1)
+        show(node.bottomRight, lvl + 1)
+
+
+t = Solution().construct(
+    [
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+    ]
+)
+
+# t = Solution().construct([[1,0,1,0,1,0,e1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]])
+show(t)
