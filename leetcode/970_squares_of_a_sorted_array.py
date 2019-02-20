d06f56cc43dccd07a3d88d999ddb0f8d9f2480c1
Karol Kruzelecki <karol@kruzelecki.com>
2019-02-20 21:11:27 +0100

    LeetCode: 970 - added solution from LeetCode forum

--- leetcode/970_squares_of_a_sorted_array.py
+++ leetcode/970_squares_of_a_sorted_array.py
@@ -35,7 +35,20 @@ class Solution:
                 i -= 1
         return ans
 
-
+# # LeetCode forum solution:
+# class Solution:
+#     def sortedSquares(self, A):
+#         answer = [0] * len(A)
+#         l, r = 0, len(A) - 1
+#         while l <= r:
+#             left, right = abs(A[l]), abs(A[r])
+#             if left > right:
+#                 answer[r - l] = left * left
+#                 l += 1
+#             else:
+#                 answer[r - l] = right * right
+#                 r -= 1
+#         return answer
 
 
 class BasicTest(unittest.TestCase):
