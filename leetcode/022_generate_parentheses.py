2ccd129a73e9de69e11491ddbff4a83a7829bbb5
Karol Kruzelecki <karol@kruzelecki.com>
2019-02-25 08:18:11 +0100

    LeetCode: 22, improved backtracking

--- leetcode/022_generate_parentheses.py
+++ leetcode/022_generate_parentheses.py
@@ -34,6 +34,27 @@ class Solution:  # 52 ms (32.69%)
         return result
 
 
+class Solution:  # 48 ms (40.10%)
+    def generateParenthesis(self, n: 'int') -> 'List[str]':
+        if n == 0: return []
+        result = []
+        def helper(stack, open_, close_):
+            if open_ == close_ == n:
+                result.append("".join(stack))
+            else:
+                if open_ < n:
+                    # try opening a new one
+                    stack.append("(")
+                    helper(stack, open_ + 1, close_)
+                    stack.pop()
+                if close_ < open_:
+                    # try closing
+                    stack.append(")")
+                    helper(stack, open_, close_ + 1)
+                    stack.pop()
+        helper([], 0, 0)
+        return result
+
 class BasicTest(unittest.TestCase):
     def test_1(self):
         input_ = 1
