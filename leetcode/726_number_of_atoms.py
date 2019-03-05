723bf4b2af33a3ac40bcb68274e46a110ea6e680
Karol Kruzelecki <karol@kruzelecki.com>
2019-03-05 20:15:59 +0100

    LeetCode: 726 again

--- leetcode/726_number_of_atoms.py
+++ leetcode/726_number_of_atoms.py
@@ -3,11 +3,19 @@ import unittest
 
 """
 Given a chemical formula (given as a string), return the count of each atom.
-An atomic element always starts with an uppercase character, then zero or more lowercase letters, representing the name.
-1 or more digits representing the count of that element may follow if the count is greater than 1. If the count is 1, no digits will follow. For example, H2O and H2O2 are possible, but H1O2 is impossible.
-Two formulas concatenated together produce another formula. For example, H2O2He3Mg4 is also a formula.
-A formula placed in parentheses, and a count (optionally added) is also a formula. For example, (H2O2) and (H2O2)3 are formulas.
-Given a formula, output the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+An atomic element always starts with an uppercase character, then zero or more
+lowercase letters, representing the name.
+1 or more digits representing the count of that element may follow if the
+count is greater than 1. If the count is 1, no digits will follow. For
+example, H2O and H2O2 are possible, but H1O2 is impossible.
+Two formulas concatenated together produce another formula. For example,
+H2O2He3Mg4 is also a formula.
+A formula placed in parentheses, and a count (optionally added) is also
+a formula. For example, (H2O2) and (H2O2)3 are formulas.
+Given a formula, output the count of all elements as a string in the
+following form: the first name (in sorted order), followed by its count
+(if that count is more than 1), followed by the second name (in sorted order),
+followed by its count (if that count is more than 1), and so on.
 """
 
 
@@ -71,6 +79,31 @@ class Solution:
         d = count(formula)
         if '' in d: del d['']
         return "".join(["{}{}".format(k, d[k]) if d[k]>1 else k for k in sorted(d.keys())])
+
+## Best LeetCode solution, but it fails on one of the tests
+# import collections
+# class Solution:
+#     def countOfAtoms(self, formula):
+#         dic, coeff, stack, elem, cnt, i = collections.defaultdict(int), 1, [], "", 0, 0  
+#         for c in formula[::-1]:
+#             if c.isdigit():
+#                 cnt += int(c) * (10 ** i)
+#                 i += 1
+#             elif c == ")":
+#                 stack.append(cnt)
+#                 coeff *= cnt
+#                 i = cnt = 0
+#             elif c == "(":
+#                 coeff //= stack.pop()
+#                 i = cnt = 0
+#             elif c.isupper():
+#                 elem += c
+#                 dic[elem[::-1]] += (cnt or 1) * coeff
+#                 elem = ""
+#                 i = cnt = 0
+#             elif c.islower():
+#                 elem += c
+#         return "".join(k + str(v > 1 and v or "") for k, v in sorted(dic.items()))
 
 
 class BasicTest(unittest.TestCase):
