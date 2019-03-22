ab87cfb0d13aaf598ee012a11d27ac79a7a2d932
Karol Kruzelecki <karol@kruzelecki.com>
2019-03-22 18:29:45 +0100

    LeetCode: 551 - improvements

--- leetcode/551_student_attendance_record_1.py
+++ leetcode/551_student_attendance_record_1.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import *
 
 class Solution:  # Very slow comparing to other submissions :( why?
     def checkRecord(self, s):
@@ -18,6 +19,20 @@ class Solution:  # Very slow comparing to other submissions :( why?
             day_min2, day_min1 = day_min1, day
         return True
 
+class Solution:  
+    def checkRecord(self, s: str) -> bool:
+        A_count = L_count = 0
+        for i, v in enumerate(s):
+            if v == "L":
+                L_count += 1
+            else:
+                L_count = 0
+                if v == "A":
+                    A_count += 1
+            if L_count > 2 or A_count > 1:
+                return False
+        return True
+            
         
 
 class BasicTest(unittest.TestCase):
